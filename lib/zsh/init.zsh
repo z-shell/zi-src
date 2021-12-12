930520e01cd15019e1357e5db3c04b90202bb0cf
Salvydas Lukosius <sall@w-ss.io>
2021-12-12 06:35:54 +0000

    modified:   lib/zsh/init.zsh

--- lib/zsh/init.zsh
+++ lib/zsh/init.zsh
@@ -2,8 +2,7 @@
 # Variables:
 local repo="https://github.com/z-shell/zi.git"
 local branch="main"
-# Verbose output
-#local verbose_mode="${verbose_mode:-1}"
+local verbose_mode="${verbose_mode:-false}"
 # ZI variables:
 declare -A ZI
 # Where ZI should create all working directories, e.g.: "~/.zi"
@@ -15,21 +14,16 @@ ZI[ZCOMPDUMP_PATH]="${HOME}/.zcompdump"
 # If set to 1, then mutes some of the ZI warnings, specifically the plugin already registered warning
 ZI[MUTE_WARNINGS]='0'
 
+# Clone ZI repository if it doesn't exist
 zzsetup() {
-  if [[ $verbose_mode ]] {
-    builtin print "(ZI): Checking if ZI (zi.zsh) is available."
-  }
+  [[ $verbose_mode == true ]] && builtin print "(ZI): Checking if ZI (zi.zsh) is available."
   if [[ ! -f "${ZI[BIN_DIR]}/zi.zsh" ]]; then
-    if [[ $verbose_mode ]] {
-      builtin print "(ZI): ZI (zi.zsh) is not found. Installing..."
-    }
+    [[ $verbose_mode == true ]] && builtin print "(ZI): ZI (zi.zsh) is not found. Installing..."
   builtin print -P "%F{33}▓▒░ %F{160}Installing interactive feature-rich plugin manager (%F{33}z-shell/zi%F{160})%f%b"
   command mkdir -p "${ZI[BIN_DIR]}" && command chmod g-rwX "${ZI[BIN_DIR]}"
   command git clone -q --progress --branch "$branch" "$repo" "${ZI[BIN_DIR]}"
     if [[ -f "${ZI[BIN_DIR]}/zi.zsh" ]]; then
-      if [[ $verbose_mode ]] {
-        builtin print "(ZI): Installed and ZI (zi.zsh) is found"
-      }
+      [[ $verbose_mode == true ]] && builtin print "(ZI): Installed and ZI (zi.zsh) is found"
       git_refs=("${(@f)$(cd "${ZI[BIN_DIR]}"; command git for-each-ref --format="%(refname:short):%(subject)" refs/heads refs/tags)}")
       print -P "%F{33}▓▒░ %F{34}Successfully installed %F{160}(%F{33}z-shell/zi%F{160})%f%b"
       print -P "%F{33}▓▒░ %F{34}Last changes:%f%b"
@@ -44,29 +38,24 @@ zzsetup() {
   fi
 }
 
+# Load ZI
 zzsource() {
-  if [[ $verbose_mode ]] {
-    builtin print "(ZI): Loading ZI (zi.zsh)"
-  }
+  [[ $verbose_mode == true ]] && builtin print "(ZI): Loading ZI (zi.zsh)"
   source "${ZI[BIN_DIR]}/zi.zsh"
 }
 
+# Enebale completion
 zzcomps() {
-  if [[ $verbose_mode ]] {
-    builtin print "(ZI): Loading ZI (_zi) completion… (_zi)"
-  }
+  [[ $verbose_mode == true ]] && builtin print "(ZI): Loading ZI (_zi) completion… (_zi)"
   autoload -Uz _zi
   (( ${+_comps} )) && _comps[zi]=_zi
 }
 
+# If repositor available, then load it and enable completion.
 zzinit() {
-  if [[ $verbose_mode ]] {
-    builtin print "(ZI): Checking if (zi_setup) function status code is 0, before sourcing ZI (zi.zsh)"
-  }
-  if [[ zzsetup ]]; then
-    if [[ $verbose_mode ]] {
-      builtin print "(ZI): Loading ZI (zi.zsh)"
-    }
+  [[ $verbose_mode == true ]] && builtin print "(ZI): Checking if (zi_setup) function status code is 0, before sourcing ZI (zi.zsh)"
+  if zzsetup; then
+    [[ $verbose_mode == true ]] && builtin print "(ZI): Loading ZI (zi.zsh)"
     zzsource
     zzcomps
     else
