eecd32d815c9bc7bf36cc6bdc1a97b59e844e602
Salvydas Lukosius <sall@w-ss.io>
2021-12-12 05:17:19 +0000

    Update init.zsh

--- lib/zsh/init.zsh
+++ lib/zsh/init.zsh
@@ -14,22 +14,22 @@ ZI[ZCOMPDUMP_PATH]="${HOME}/.zcompdump"
 # If set to 1, then mutes some of the ZI warnings, specifically the plugin already registered warning
 ZI[MUTE_WARNINGS]='0'
 
-git_exec() { builtin cd "${ZI[BIN_DIR]}"; command git "${@}"; }
+git_exec() { command git -C "${ZI[BIN_DIR]}" "${@}"; }
 zzversion() { git_exec describe --tags 2>/dev/null; }
 zzsetup() {
   if [[ $ZI_VERBOSE = on ]]; then
-    echo "(ZI): Checking if ZI (zi.zsh) is available."
+    builtin print "(ZI): Checking if ZI (zi.zsh) is available."
   fi
   if [[ ! -f "${ZI[BIN_DIR]}/zi.zsh" ]]; then
     if [[ $ZI_VERBOSE = on ]]; then
-      echo "(ZI): ZI (zi.zsh) is not found. Installing..."
+      builtin print "(ZI): ZI (zi.zsh) is not found. Installing..."
     fi
-  print -P "%F{33}▓▒░ %F{160}Installing interactive feature-rich plugin manager (%F{33}z-shell/zi%F{160})%f%b"
+  builtin print -P "%F{33}▓▒░ %F{160}Installing interactive feature-rich plugin manager (%F{33}z-shell/zi%F{160})%f%b"
   command mkdir -p "${ZI[BIN_DIR]}" && command chmod g-rwX "${ZI[BIN_DIR]}"
   command git clone -q --progress --branch "$ZI_BRANCH" "$ZI_REPO" "${ZI[BIN_DIR]}"
     if [[ -f "${ZI[BIN_DIR]}/zi.zsh" ]]; then
       if [[ $ZI_VERBOSE = on ]]; then
-        echo "(ZI): Installed and ZI (zi.zsh) is found"
+        builtin print "(ZI): Installed and ZI (zi.zsh) is found"
       fi
       print -P "%F{33}▓▒░ %F{34}Successfully installed %F{160}(%F{33}z-shell/zi%F{160}) %F{34} Version:%F{160} (%F{33}$(zzversion)%F{160})%f%b"
     else
@@ -44,14 +44,14 @@ zzsetup() {
 
 zzsource() {
   if [[ $ZI_VERBOSE = on ]]; then
-    echo "(ZI): Loading ZI (zi.zsh)"
+    builtin print "(ZI): Loading ZI (zi.zsh)"
   fi
   source "${ZI[BIN_DIR]}/zi.zsh"
 }
 
 zzcomps() {
   if [[ $ZI_VERBOSE = on ]]; then
-    echo "(ZI): Loading ZI (_zi) completion… (_zi)"
+    builtin print "(ZI): Loading ZI (_zi) completion… (_zi)"
   fi
   autoload -Uz _zi
   (( ${+_comps} )) && _comps[zi]=_zi
@@ -59,11 +59,11 @@ zzcomps() {
 
 zzinit() {
   if [[ $ZI_VERBOSE = on ]]; then
-    echo "(ZI): Checking if (zi_setup) function status code is 0, before sourcing ZI (zi.zsh)"
+    builtin print "(ZI): Checking if (zi_setup) function status code is 0, before sourcing ZI (zi.zsh)"
   fi
   if zzsetup; then
   if [[ ${ZI_VERBOSE} = on ]]; then
-    echo "(ZI): Loading ZI (zi.zsh)"
+    builtin print "(ZI): Loading ZI (zi.zsh)"
   fi
     zzsource
     zzcomps
