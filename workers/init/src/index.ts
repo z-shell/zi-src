d2b11b1a7ce2ee1904b79c1d062abf2bae30ac2c
Salvydas Lukosius <sall@w-ss.io>
2022-06-09 01:00:40 +0100

    ci(workspace): 🚀 Setup Cloudflare workers [skip ci]

--- workers/init/src/index.ts
+++ workers/init/src/index.ts
@@ -2,7 +2,7 @@ const destinationURL =
   "https://raw.githubusercontent.com/z-shell/zi-src/main/lib/zsh/init.zsh";
 const statusCode = 301;
 
-async function handleRequest(request) {
+async function handleRequest(_request: Request) {
   return Response.redirect(destinationURL, statusCode);
 }
 
