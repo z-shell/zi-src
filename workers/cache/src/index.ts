989b85abc0d37f2daecb7e19ef9d18559a4f20df
Salvydas Lukosius <sall@w-ss.io>
2022-07-16 02:42:41 +0100

    modified:   .trunk/.gitignore 	modified:   .trunk/trunk.yaml 	modified:   workers/cache/pnpm-lock.yaml 	modified:   workers/cache/src/index.ts 	deleted:    workers/r2-cache/package.json 	deleted:    workers/r2-cache/pnpm-lock.yaml 	deleted:    workers/r2-cache/src/index.ts 	deleted:    workers/r2-cache/tsconfig.json 	deleted:    workers/r2-cache/wrangler.toml

--- workers/cache/src/index.ts
+++ workers/cache/src/index.ts
@@ -21,11 +21,8 @@ async function handleRequest(event) {
     // Must use Response constructor to inherit all of response's fields
     response = new Response(response.body, response);
 
-    // Cache API respects Cache-Control headers. Setting s-max-age to 10
-    // will limit the response to be in cache for 10 seconds max
-
     // Any changes made to the response here will be reflected in the cached value
-    response.headers.append("Cache-Control", "s-maxage=10");
+    response.headers.append("Cache-Control", "s-maxage=120");
 
     // Store the fetched response as cacheKey
     // Use waitUntil so you can return the response without blocking on
