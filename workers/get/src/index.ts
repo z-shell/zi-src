5cf6446dccee9b4bf22b0986176471f755738ca9
Salvydas Lukosius <sall@w-ss.io>
2022-09-30 20:07:06 +0100

    [feat]: Serve install script from R2  	renamed:    .markdownlint.yaml -> .trunk/config/.markdownlint.yaml 	modified:   .trunk/trunk.yaml 	modified:   workers/get/src/index.ts

--- workers/get/src/index.ts
+++ workers/get/src/index.ts
@@ -1,15 +1,9 @@
-addEventListener("fetch", async (event) => {
-  const request = event.request;
-  try {
-    return event.respondWith(handleRequest(request));
-  } catch (e) {
-    return event.respondWith(new Response(`Error thrown ${e.message}`));
-  }
-});
+export default {
+  fetch(request) {
+    const base = "https://r2.zshell.dev/src/sh/install.sh";
+    const statusCode = 301;
 
-async function handleRequest(request) {
-  const destinationURL =
-    "https://raw.githubusercontent.com/z-shell/zi-src/main/lib/sh/install.sh";
-  const statusCode = 301;
-  return Response.redirect(destinationURL, statusCode);
-}
+    const destination = new URL(request.url, base);
+    return Response.redirect(destination.toString(), statusCode);
+  },
+};
